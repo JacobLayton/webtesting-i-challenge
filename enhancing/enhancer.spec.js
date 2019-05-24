098cb461566c36117e9dda0a1fac313ce5a66206
JacobLayton <jlayjlay3@gmail.com>
2019-05-23 18:23:50 -0700

    Succeed test passes

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -11,3 +11,15 @@ describe("repair", () => {
     expect(repair.durability).toBe(100);
   });
 });
+
+describe("succeed", () => {
+  it("Increases enhancement by 1", () => {
+    const item = {
+      name: "Sword",
+      durability: 50,
+      enhancement: 10
+    };
+    const enhance = enhancer.succeed(item);
+    expect(enhance.enhancement).toBe(11);
+  });
+});
