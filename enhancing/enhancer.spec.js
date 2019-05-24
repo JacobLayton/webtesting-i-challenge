adaa8b2768f6a7f36cc609c6f86058ab00911ab6
JacobLayton <jlayjlay3@gmail.com>
2019-05-23 18:17:23 -0700

    Repair test complete and passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,7 +1,13 @@
 const enhancer = require("./enhancer.js");
 // test away!
-it("repairs to full durability", () => {
-  const item = {
-    name: ""
-  };
+describe("repair", () => {
+  it("repairs to full durability", () => {
+    const item = {
+      name: "Sword",
+      durability: 50,
+      enhancement: 10
+    };
+    const repair = enhancer.repair(item);
+    expect(repair.durability).toBe(100);
+  });
 });
